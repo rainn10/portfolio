96cdda2f046701a384ee3742cf3b43642d3cc818
rainn10 <147773343+rainn10@users.noreply.github.com>
2023-11-29 20:23:29 -0700

    alert hover button

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,26 @@ copyrightYear.textContent = `© Rain Aclao, ${currentYear}`;
 
 const alertButton = document.getElementById("btn-alert");
 
+// Set the default button label
+const defaultLabel = 'Do Not Click!!!';
+
+// Set the new button label for hover
+const hoverLabel = 'New Button Label';
+
+// Set the default label initially
+alertButton.innerText = defaultLabel;
+
+// Change the button label on mouseover
+alertButton.addEventListener("mouseover", function () {
+    alertButton.innerText = hoverLabel;
+});
+
+// Revert the button label on mouseleave
+alertButton.addEventListener("mouseleave", function () {
+    alertButton.innerText = defaultLabel;
+});
+
+// Click event listener
 alertButton.addEventListener("click", function () {
-    alert("Hello world!");
+    alert("you are stubborn");
 });
