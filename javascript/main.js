9a0d5c9f3a103164579cbd27755eb1658abaf96c
rainn10 <147773343+rainn10@users.noreply.github.com>
2023-11-29 20:04:34 -0700

    functional alert button

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,10 @@
-//footer
-document.addEventListener("DOMContentLoaded", function () {
-    const currentYear = new Date().getFullYear();
-    const copyrightYear = document.getElementById("cc");
-    copyrightYear.textContent = `© Rain Aclao, ${currentYear}`;
+/*footer*/
+const currentYear = new Date().getFullYear();
+const copyrightYear = document.getElementById("cc");
+copyrightYear.textContent = `© Rain Aclao, ${currentYear}`;
+
+const alertButton = document.getElementById("btn-alert");
+
+alertButton.addEventListener("click", function () {
+    alert("Hello world!");
 });
